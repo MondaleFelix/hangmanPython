544483df0278d3e556b86c3752ec90881c6ffc9f
Mondale Felix <mondalefelix@yahoo.com>
2017-09-26 21:27:36 -0700

    cleaned up code

--- hangman2.py
+++ hangman2.py
@@ -27,58 +27,22 @@ def guessLetter():
       isWordGuessed(secretWord, printedWord)
       guessLetter()
 
-
-
-
-
-    # for i in secretWord:
-    #   if letter == i:
-       # printedWord.pop(secretWord.index(letter))
-       # printedWord.insert(secretWord.index(letter), letter)
-
-    # goes through every single character in secretWord
-
-
 def isWordGuessed(secretWord, printedWord):
     if secretWord == printedWord:
       print(printedWord)
       print("Winner")
     elif guesses == 0:
-      print("loser")
-
-    '''
-    secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
-    lettersGuessed: list of letters that have been guessed so far.
-    returns: boolean, True only if all the letters of secretWord are in lettersGuessed;
-      False otherwise
-    '''
-    # FILL IN YOUR CODE HERE...
-
-
+      print("Fucking loser")
 
 
 def getGuessedWord(secretWord):
     for i in secretWord:
       printedWord.append("_")
     guessLetter()
-    '''
-    secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
-    lettersGuessed: list of letters that have been guessed so far.
-    returns: string, of letters and underscores.  For letters in the word that the user has
-    guessed correctly, the string should contain the letter at the correct position.  For letters
-    in the word that the user has not yet guessed, shown an _ (underscore) instead.
-    '''
-    # FILL IN YOUR CODE HERE...
-
 
 
 
 def getAvailableLetters(lettersGuessed):
-    '''
-    lettersGuessed: list of letters that have been guessed so far
-    returns: string, comprised of letters that represents what letters have not
-      yet been guessed.
-    '''
 
 
 
